70f7736217b5fa6f6f6ee18b8568378941570d0f
VaricelaZoster <162027550+VaricelaZoster@users.noreply.github.com>
2026-04-26 01:39:16 +0530

    Initial Navbar done

---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+'use client'
+import React from 'react'
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { useRouter } from 'next/navigation'
+
+const Navbar = () => {
+
+    const router = useRouter()
+
+    return (
+        <div className='fixed top-10 z-10 w-[94%] bg-white h-12 md:h-21 mx-17 px-15 py-2 rounded-full flex items-center justify-between'>
+            <div className='flex items-center gap-10 '>
+                <div className='flex text-xl md:text-4xl font-medium cursor-pointer items-center' onClick={() => router.push('/')}>
+                    <div>Linktree</div>
+                </div> 
+                <div className='group hidden [@media(min-width:1133px)]:flex text-semibold font-small gap-6 items-center'>
+                    <div className='p-2.5 rounded-md duration-200 cursor-pointer hover:bg-gray-200 '>Products</div>
+                    <div className='p-2.5 rounded-md duration-200 cursor-pointer hover:bg-gray-200 '>Templates</div>
+                    <div className='p-2.5 rounded-md duration-200 cursor-pointer hover:bg-gray-200 '> Marketplace</div>
+                    <div className='p-2.5 rounded-md duration-200 cursor-pointer hover:bg-gray-200 '>Learn</div>
+                    <div className='p-2.5 rounded-md duration-200 cursor-pointer hover:bg-gray-200 '>Pricing</div>
+                </div>
+            </div>
+            <div className='flex justify-between gap-3 text-sm md:text-lg font-[500]'>
+                <div onClick={() => router.push('/login')} className='bg-[#eff0ec] hover:bg-[#e9e9e9] flex items-center md:p-4 md:px-6 rounded-lg duration-150 cursor-pointer'>Log in</div>
+                <div onClick={() => router.push('/signup')} className='bg-[#1e2330] hover:bg-[#262d3e] flex items-center p-4 px-6 rounded-full text-white cursor-pointer duration-150'>Sign up free</div>
+                <div className='flex items-center flex [@media(min-width:1133px)]:hidden'><Bars3Icon className='h-9'/></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
